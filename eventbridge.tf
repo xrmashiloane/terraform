dac6632116409e9e18f104f34889a6f826635951
Reagan M <reagan.mashiloane@gmail.com>
2023-10-16 16:45:27 +0200

    Added second schedule

--- eventbridge.tf
+++ eventbridge.tf
@@ -9,12 +9,18 @@ module "eventbridge" {
 
   schedules = {
     lambda-cron = {
-      description         = "Trigger for a Lambda"
-      schedule_expression = "rate(1 day)"
+      description         = "Morning trigger for Lambda"
+      schedule_expression = "cron(0 9 ? * MON-FRI *)"
       timezone            = "Africa/Johannesburg"
       arn                 = aws_lambda_function.hello.arn
-      input               =  jsonencode({"query": "Durban"})
-      
+      input               =  jsonencode({"query": "Johannesburg"})
+    }
+      lambda-cron = {
+      description         = "Evening trigger for Lambda"
+      schedule_expression = "cron(0 16 ? * MON-FRI *)"
+      timezone            = "Africa/Johannesburg"
+      arn                 = aws_lambda_function.hello.arn
+      input               =  jsonencode({"query": "Johannesburg"})
     }
   }
 }
