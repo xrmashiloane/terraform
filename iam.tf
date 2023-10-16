aafdf1a3f32c578c889ec902d298d59102b21046
Reagan M <reagan.mashiloane@gmail.com>
2023-10-16 18:18:58 +0200

    Update

--- iam.tf
+++ iam.tf
@@ -19,29 +19,16 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
   }
   statement {
     actions = ["ssm:GetParameter"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-
-    }
     resources = [aws_ssm_parameter.api_access_key.arn]
     effect = "Allow"
   }
   statement {
     actions = ["sns:Publish"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
     resources = [aws_sns_topic.results_updates.arn]
     effect = "Allow"
   }
   statement {
     actions = ["kms:Decrypt"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
     resources = ["arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/*"]
     effect = "Allow"
   }
