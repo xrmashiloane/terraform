993466140126055c5662ce97c560bedcf0646d10
Reagan M <reagan.mashiloane@gmail.com>
2023-10-16 20:12:28 +0200

    Update iam module

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+#Lamda Execution Role
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -18,7 +19,13 @@ resource "aws_iam_role" "iam_for_lambda" {
   })
 }
 
-data "aws_iam_policy_document" "sns_policy_doc" {
+#Get account details for use with KMS policy 
+
+data "aws_caller_identity" "current" {}
+
+#Update role with permission to send SNS Notifications
+
+data "aws_iam_policy_document" "lambda_policy_doc" {
   statement {
     sid       = "SendNotification"
     actions   = [
@@ -28,14 +35,33 @@ data "aws_iam_policy_document" "sns_policy_doc" {
       aws_sns_topic.results_updates.arn
     ]
   }
+  statement {
+    sid       = "DecryptKMS"
+    actions   = [
+      "kms:Decrypt"
+    ]
+    resources = [
+      "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/*"
+    ]
+  }
+  statement {
+    sid       = "GetSecureParameter"
+    actions   = [
+      "ssm:GetParameter"
+    ]
+    resources = [
+      aws_ssm_parameter.api_access_key.arn
+    ]
+  }
 }
 
-resource "aws_iam_policy" "sns_send_notification" {
-  name   = "${var.project_name}-send-notification"
-  policy = data.aws_iam_policy_document.sns_policy_doc.json
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "${var.project_name}-policy"
+  policy = data.aws_iam_policy_document.lambda_policy_doc.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.sns_send_notification.arn
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
