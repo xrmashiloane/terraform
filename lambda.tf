9c00b076caca6df3f5be85b1ef3387297860986a
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 19:31:53 +0200

    Update lambda

--- lambda.tf
+++ lambda.tf
@@ -12,6 +12,7 @@ resource "aws_lambda_function" "hello" {
 
   function_name = var.project_name
   role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.11"
   timeout       = 10
   layers        = [aws_lambda_layer_version.lambda_layer.arn]
