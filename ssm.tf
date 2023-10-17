bef6b889eaba0d078636d730d46be9b3778031ea
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 21:12:03 +0200

    Get key

--- ssm.tf
+++ ssm.tf
@@ -1,6 +1,6 @@
 #Create Secure String
 resource "aws_ssm_parameter" "api_access_key" {
-  name        = "api_access_key"
+  name        = "api_access_key_value"
   description = "Access key for API"
   type        = "SecureString"
   value       = var.api_access_key_value
