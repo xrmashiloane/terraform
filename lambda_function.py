240c0529081a15fc9fac81a40e53c8fc67d92aca
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 20:19:02 +0200

    message print

--- lambda_function.py
+++ lambda_function.py
@@ -2,13 +2,8 @@ import json
 
 
 def lambda_handler(event, context):
-
-    for record in event['Records']:     
-        #pull the body out & json load it
-        body_rec = record['body']
-        body_rec = json.loads(body_rec)
         
-        message = body_rec['Records'][0]['body']
+        message = event['Records'][0]['body']
         print(message)
         return message
         
