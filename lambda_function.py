8ca9906c1168a7e0c5def7ea8e0664e01f354b4c
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 20:39:30 +0200

    Refacror

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,40 @@
 import json
-
+import requests
+import boto3
 
 def lambda_handler(event, context):
+
         
-        message = event['Records'][0]['body']
+        message = event['Records'][0]['body']['query']
         print(message)
-        return message
+
+        ssm_client = boto3.client('ssm')
+    
+        def get_parameter(name):
+            parameter = ssm_client.get_parameter(
+                Name=name,
+                WithDecryption=True                           
+                )
+            return parameter['Parameter']['Value']
+    
+
+        params = {
+            "access_key": get_parameter('access_key'),
+            "query": message
+        }
+
+        data = requests.get('http://api.weatherstack.com/current', params=params).json()
+        # Save results to dynamo db
+    
+        if data:
+            location = data['location']['name']
+            region = data['location']['region']
+            country = data['location']['country']
+            temperature = data['current']['temperature']
+            feels = data['current']['feelslike']
+            condition = data['current']['weather_descriptions'][0]
+        else:
+            return print("Error fetching data")
+
+        return data
         
