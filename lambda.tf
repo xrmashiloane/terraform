8302f15831e07227127ac76cd77cb57e4788ed16
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 19:46:08 +0200

    Refactor

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "zip" {
 }
 
 #Create Lambda function and related aliases
-resource "aws_lambda_function" "hello" {
+resource "aws_lambda_function" "function" {
   filename         = data.archive_file.zip.output_path
   source_code_hash = filebase64sha256(data.archive_file.zip.output_path)
 
@@ -22,14 +22,14 @@ resource "aws_lambda_function" "hello" {
 resource "aws_lambda_alias" "alias_dev" {
   name             = "dev"
   description      = "dev"
-  function_name    = aws_lambda_function.hello.arn
+  function_name    = aws_lambda_function.function.arn
   function_version = "$LATEST"
 }
 
 resource "aws_lambda_alias" "alias_prod" {
   name             = "prod"
   description      = "prod"
-  function_name    = aws_lambda_function.hello.arn
+  function_name    = aws_lambda_function.function.arn
   function_version = "$LATEST"
 }
 
@@ -44,13 +44,13 @@ resource "aws_lambda_layer_version" "lambda_layer" {
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.sqs_queue.arn
   enabled          = true
-  function_name    = aws_lambda_function.hello.arn
+  function_name    = aws_lambda_function.function.arn
   batch_size       = 1
 }
 
 #Create Cloudwatch log group for Lambda
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name = "/aws/lambda/${aws_lambda_function.hello.function_name}"
+  name = "/aws/lambda/${aws_lambda_function.function.function_name}"
   retention_in_days = 7
   lifecycle {
     prevent_destroy = false
@@ -59,7 +59,7 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 
 #Send function results to SNS topic for delivery to subscribers
 resource "aws_lambda_function_event_invoke_config" "send_notification" {
-  function_name = aws_lambda_function.hello.arn
+  function_name = aws_lambda_function.function.arn
   destination_config {
     on_failure {
       destination = aws_sns_topic.results_updates.arn
