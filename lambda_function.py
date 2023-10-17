de3f05b9d77033fe7543bc037a0a0defff7af098
Reagan M <reagan.mashiloane@gmail.com>
2023-10-17 19:37:32 +0200

    Function updates

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,8 @@ import json
 
 def lambda_handler(event, context):
     
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
+    print(event)
+
+    print("---------")
+
+    print(context)
