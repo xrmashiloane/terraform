200a004ae0099c6779d560d0826414f4ab4d4743
Reagan <reagan.mashiloane@gmail.com>
2023-10-18 13:16:24 +0200

    Destroy

---
+++ terraform.tf
@@ -0,0 +1,27 @@
+#Required variables for Terraform Cloud
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  #Only required for Terraform Cloud runs
+  cloud {
+    organization = "xrmashiloane"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "af-south-1"
+}
+
+#Declare API key as secret variable to prevent leakage
+variable "api_access_key_value" {
+  type = string
+  sensitive = true
+}
