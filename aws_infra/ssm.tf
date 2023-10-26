67f17dbbf8f988f00c78587d4fb8772742ce7fcb
Reagan <reagan.mashiloane@gmail.com>
2023-10-27 01:17:11 +0200

    Format

--- aws_infra/ssm.tf
+++ aws_infra/ssm.tf
@@ -8,8 +8,8 @@ resource "aws_ssm_parameter" "api_access_key_value" {
 
 #Create DynamoDB table Name Parameter
 resource "aws_ssm_parameter" "dynamodb_table_name" {
-  name = "dynamodb_table"
+  name        = "dynamodb_table"
   description = "Created DynamoDB table name for project"
-  type = "String"
-  value = aws_dynamodb_table.dynamodb-table.name
+  type        = "String"
+  value       = aws_dynamodb_table.dynamodb-table.name
 }
