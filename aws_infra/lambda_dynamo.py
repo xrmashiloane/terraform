7de7c67275fe6240a0ecbfdeba11a30543341e3e
Reagan Mashiloane <reagan.mashiloane@gmail.com>
2024-02-13 21:29:10 +0200

    Update sqs message logic

--- aws_infra/lambda_dynamo.py
+++ aws_infra/lambda_dynamo.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+import uuid
+from typing import List
 
 def lambda_handler(event, context):
     '''Initialise required boto3 clients'''
@@ -37,15 +39,22 @@ def lambda_handler(event, context):
     def create_sqs_list(dataset):
         '''Create list of messages to send to SQS'''
         sqs_messages = []
-        print(dataset)
-        for loc in dataset:
-            location = loc['city']['S']
-            sqs_messages.append(
-                QueueUrl=sqs_parameter,
-                MessageBody=location
-            )
+        for item in dataset:
+            
+            sqs_messages.append( location = item['city']['S'])
         return sqs_messages
-    #Send Message
-    response = boto3.client('sqs').send_message_batch(create_sqs_list(dataset))
+    
+    def send_sqs_messages(sqs_messages, sqs_parameter):
+        '''Send messages to SQS'''
+        response = boto3.client('sqs').send_message_batch(
+            QueueUrl=sqs_parameter,
+            Entries=sqs_messages
+        )
+        return response
 
-    print(response)
+    sqs_messages = create_sqs_list(dataset)
+    print(sqs_messages)
+
+    send_sqs_messages(sqs_messages, sqs_parameter)
+
+
